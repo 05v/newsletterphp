1617176acf46bf74cff8ea93864eedd41af7b50c
vladverheij <vladverheij@gmail.com>
2023-09-21 10:15:58 +0200

    add form.php and link to index.php, change .gitignore to ignore mysqldata folder

---
+++ source/views/form.php
@@ -0,0 +1,14 @@
+<div class="newsletter">
+    <form class="newsletter__form">
+        <label class="newsletter__label" for="email">Email:</label>
+        <input class="newsletter__input" type="email" id="email" name="email" required>
+
+        <label class="newsletter__label" for="newsletter-type">Selecteer nieuwsbrief type:</label>
+        <select class="newsletter__select" id="newsletter-type" name="newsletter-type" required>
+            <option class="newsletter__option" value="type1">Nieuwsbrief Type 1</option>
+            <option class="newsletter__option" value="type2">Nieuwsbrief Type 2</option>
+        </select>
+
+        <button class="newsletter__submit" type="submit">Verzenden</button>
+    </form>
+</div>
